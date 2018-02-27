a5505ddd7c0a5771111298b5caddf8bae1ac0210
OlegMarko <olegmarko7@gmail.com>
2018-02-27 23:27:54 +0200

    Make index page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myTopNav() {
+    var x = document.getElementById('my-top-nav');
+
+    if (x.className === 'top-nav') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'top-nav';
+    }
+};
